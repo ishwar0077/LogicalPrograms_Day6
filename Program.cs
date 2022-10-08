2c77ee7f9efc72a9bb81b804430d81b515af5da0
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-08 20:09:56 +0530

    reverse the Number

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LogicalPrograms
     {
         public static void Main(string[] args)
         {
-            PrimeNumber.checkPrime();
+            ReverseNumber.ReverseTheNumber();
             
            
         }
